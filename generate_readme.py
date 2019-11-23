8d2f6de34aa0513e0cb84fc3c31fc8ec5cf91491
Josh Long <jlong@Joshuas-MacBook-Pro.local>
2019-11-23 00:12:15 -0800

    up

--- generate_readme.py
+++ generate_readme.py
@@ -5,9 +5,9 @@ import os, sys, time, re
 if __name__ == '__main__' :
     root = 'this-week-in'
     readme_txt = '''
-    mkdir %s && cd %s && curl https://raw.githubusercontent.com/%s/__init__/master/start.sh | bash
+mkdir %s && cd %s && curl https://raw.githubusercontent.com/%s/__init__/master/start.sh | bash
     ''' % ( root,root,root)
-    readme_txt = readme_txt.strip().lstrip ()
+    readme_txt = readme_txt.strip().lstrip()
     with  open( 'README.md', 'w') as fp :
 
         fp.writelines([os.linesep])
