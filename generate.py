ea5bc6ff2a4882e481ea4da1d534f14f6433abda
Josh Long <josh@joshlong.com>
2019-11-23 00:38:02 -0800

    up

--- generate.py
+++ generate.py
@@ -26,13 +26,13 @@ REPOSITORIES_TEXT=%s
 GIT_TEMPLATE_ROOT=git@github.com:%s/
 start=$( cd `dirname $0` && pwd	 )
 echo "initializing from ${start} "
-curl https://github.com/%s/__init__/blob/master/repositories.txt | while read l ; do
+curl https://raw.githubusercontent.com/%s/__init__/master/repositories.txt | while read l ; do
     d=$(echo $l | cut -f5 -d\/ | cut -f1 -d\. ) 
     echo "Processing $d"  
     dir_to_create=${start}/$d
     if [[ -e  $dir_to_create ]] ; then 
         echo "WARN: ${dir_to_create} aleady exists." 
-    else  	
+    else
         echo "initializing ${dir_to_create}"  
         git_repo=${GIT_TEMPLATE_ROOT}/${d}.git
         git clone ${git_repo} ${dir_to_create}
--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ REPOSITORIES_TEXT=https://github.com/this-week-in/__init__/blob/master/repositor
 GIT_TEMPLATE_ROOT=git@github.com:this-week-in/
 start=$( cd `dirname $0` && pwd	 )
 echo "initializing from ${start} "
-curl https://github.com/this-week-in/__init__/blob/master/repositories.txt | while read l ; do
+curl https://raw.githubusercontent.com/this-week-in/__init__/master/repositories.txt | while read l ; do
     d=$(echo $l | cut -f5 -d\/ | cut -f1 -d\. ) 
     echo "Processing $d"  
     dir_to_create=${start}/$d
     if [[ -e  $dir_to_create ]] ; then 
         echo "WARN: ${dir_to_create} aleady exists." 
-    else  	
+    else
         echo "initializing ${dir_to_create}"  
         git_repo=${GIT_TEMPLATE_ROOT}/${d}.git
         git clone ${git_repo} ${dir_to_create}
