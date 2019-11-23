ec3bafac32cc308223c474cda691fe603a2c4f53
Josh Long <josh@joshlong.com>
2019-11-23 00:27:24 -0800

    up

--- start.sh
+++ start.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash 
 
-REPOSITORIES_TEXT=https://github.com/this-week-in/__init__/blob/master/repositories.txt 
+REPOSITORIES_TEXT=https://github.com/this-week-in/__init__/blob/master/repositories.txt
 GIT_TEMPLATE_ROOT=git@github.com:this-week-in/
 start=$( cd `dirname $0` && pwd	 )
-
 echo "initializing from ${start} "
-
 curl https://github.com/this-week-in/__init__/blob/master/repositories.txt | while read l ; do
-
     d=$(echo $l | cut -f5 -d\/ | cut -f1 -d\. ) 
     echo "Processing $d"  
     dir_to_create=${start}/$d
-
     if [[ -e  $dir_to_create ]] ; then 
         echo "WARN: ${dir_to_create} aleady exists." 
     else  	
@@ -19,4 +15,4 @@ curl https://github.com/this-week-in/__init__/blob/master/repositories.txt | whi
         git_repo=${GIT_TEMPLATE_ROOT}/${d}.git
         git clone ${git_repo} ${dir_to_create}
     fi
-done 
+done
